3235d7ddf65a44ff1fcb3aa82dba1ca07a9a9339
Thanh-Macpro <>
2023-07-14 20:22:56 +0900

    add image picker

--- app/src/main/java/com/example/imageprocessing/MainActivity.kt
+++ app/src/main/java/com/example/imageprocessing/MainActivity.kt
@@ -1,80 +1,95 @@
 package com.example.imageprocessing
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
+import coil.compose.AsyncImage
 import com.example.imageprocessing.ui.theme.ImageProcessingTheme
-import com.huhx.picker.constant.AssetPickerConfig
-import com.huhx.picker.data.AssetInfo
-import com.huhx.picker.data.PickerPermissions
-import com.huhx.picker.view.AssetPicker
+import java.io.File
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            var imageUri by remember {
+                mutableStateOf<Uri?>(null)
+            }
+
+            val pickMedia =
+                rememberLauncherForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+                    if (uri != null) {
+                        val imageFile = File(uri.toString())
+                        //TODO progress imageFile
+
+                        imageUri = uri
+                    }
+                }
+
+            val launcher =
+                rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) {
+                    if (it) {
+                        pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+                    }
+                }
+
             ImageProcessingTheme {
-                ImageVisualizationScreen()
+                val context = LocalContext.current
+
+                Column(
+                    modifier = Modifier.padding(12.dp),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    Text(
+                        text = stringResource(R.string.insert_image),
+                        fontSize = 24.sp,
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    )
+                    Spacer(Modifier.height(8.dp))
+
+                    Button(onClick = {
+                        when (PackageManager.PERMISSION_GRANTED) {
+                            ContextCompat.checkSelfPermission(
+                                context,
+                                Manifest.permission.READ_EXTERNAL_STORAGE
+                            ) -> {
+                                pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+                            }
+                            else -> {
+                                launcher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                            }
+                        }
+                    }) {
+                        Text(
+                            text = "Select image ...",
+                            fontSize = 12.sp,
+                        )
+                    }
+
+                    Spacer(Modifier.height(8.dp))
+
+                    AsyncImage(
+                        model = imageUri,
+                        contentDescription = null,
+                    )
+                }
             }
         }
-    }
-}
-
-
-@Composable
-fun ImageVisualizationScreen() {
-    Column(
-        modifier = Modifier.padding(32.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        Text(
-            text = stringResource(R.string.insert_image),
-            fontSize = 24.sp,
-            modifier = Modifier.align(Alignment.CenterHorizontally)
-        )
-        Spacer(Modifier.height(16.dp))
-
-//        TODO: add a button which allow user to find image in physical device
-
-
-    }
-}
-
-@Composable
-fun ImagePicker(
-    onPicked: (List<AssetInfo>) -> Unit,
-    onClose: (List<AssetInfo>) -> Unit,
-) {
-    PickerPermissions(permissions = listOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA)) {
-        AssetPicker(
-            assetPickerConfig = AssetPickerConfig(gridCount = 3),
-            onPicked = onPicked,
-            onClose = onClose
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ImageProcessingTheme {
-        ImageVisualizationScreen()
     }
 }
